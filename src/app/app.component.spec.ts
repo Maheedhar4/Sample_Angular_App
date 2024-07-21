89adc585d738cf3b445c396f23414ad6319e40f0
Maheedhar4 <maheedarb69@gamil.com>
2024-07-21 21:09:50 +0530

    $(date1)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,28 +1,32 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { TestBed } from "@angular/core/testing";
+import { RouterTestingModule } from "@angular/router/testing";
+import { AppComponent } from "./app.component";
 
-describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule, AppComponent]
-}));
+describe("AppComponent", () => {
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule, AppComponent],
+    })
+  );
 
-  it('should create the app', () => {
+  it("should create the app", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Rent-front-end-dev'`, () => {
+  it(`should have as title 'skynet'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Rent-front-end-dev');
+    expect(app.title).toEqual("skynet");
   });
 
-  it('should render title', () => {
+  it("should render title", () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Rent-front-end-dev app is running!');
+    expect(compiled.querySelector(".content span")?.textContent).toContain(
+      "skynet app is running!"
+    );
   });
 });
